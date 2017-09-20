87468f71b4fc3409bd2bb7c174231166b0b9cef9
HanMuJiang <32116091+HanMuJiang@users.noreply.github.com>
2017-09-20 14:14:53 +0800

    Add files via upload

---
+++ hanlong-java-project-1/src/project1/Xingkong.java
@@ -0,0 +1,27 @@
+package project1;
+
+import java.awt.*;
+public class Xingkong {
+/*
+ *Õ¹Ê¾ÐÇ¿Õ³ÌÐò 
+ **/
+	public static void main(String args[]){
+		Frame f=new Frame();
+		f.setSize(1366,768);
+		f.setBackground(Color.BLACK);
+		Paint p=new Paint();
+		f.add(p);
+		f.show();
+	}
+}
+
+class Paint extends Panel{
+	public void paint(Graphics g){
+		for(int i=0;i<300;i++){
+		//g.setColor(new Color((int)(Math.random()*100),(int)(Math.random()*100),(int)(Math.random()*100)));
+		g.setColor(Color.white);
+		g.drawString("*", (int)(Math.random()*1366), (int)(Math.random()*768));
+		}
+	}
+	
+}
